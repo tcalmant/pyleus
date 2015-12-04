84927ead646e5410bf4b0531ce481a86d51063d9
Thomas Calmant <thomas.calmant@gmail.com>
2015-12-04 11:39:06 +0100

    PyleusTopologyBuilder constants are in-jar paths
    ... '/' must be used instead of File.separator

--- topology_builder/src/main/java/com/yelp/pyleus/PyleusTopologyBuilder.java
+++ topology_builder/src/main/java/com/yelp/pyleus/PyleusTopologyBuilder.java
@@ -1,12 +1,10 @@
 package com.yelp.pyleus;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
 import org.yaml.snakeyaml.error.YAMLException;
 
 import com.yelp.pyleus.bolt.PythonBolt;
@@ -36,10 +34,8 @@ import storm.kafka.StringKeyValueScheme;
 import storm.kafka.ZkHosts;
 
 public class PyleusTopologyBuilder {
-    public static final String YAML_FILENAME = StringUtils.join(
-    		new String[] {"", "resources", "pyleus_topology.yaml"}, File.separator);
-    public static final String KAFKA_ZK_ROOT_FMT = StringUtils.join(
-    		new String[] {"", "pyleus-kafka-offsets", "%s"}, File.separator);
+    public static final String YAML_FILENAME = "/resources/pyleus_topology.yaml";
+    public static final String KAFKA_ZK_ROOT_FMT = "/pyleus-kafka-offsets/%s";
     public static final String KAFKA_CONSUMER_ID_FMT = "pyleus-%s";
     public static final String MSGPACK_SERIALIZER_CLASS = MessagePackSerializer.class.getName();
     
