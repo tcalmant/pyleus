47b199eab7e230c45d2e060ea001ea3064078cff
Thomas Calmant <thomas.calmant@gmail.com>
2015-12-08 14:58:45 +0100

    Use cmd to run the Python interpreter on Windows
    This seems to avoid the "file not found" bug

--- topology_builder/src/main/java/com/yelp/pyleus/PythonComponentsFactory.java
+++ topology_builder/src/main/java/com/yelp/pyleus/PythonComponentsFactory.java
@@ -12,7 +12,8 @@ import com.yelp.pyleus.spout.PythonSpout;
 
 public class PythonComponentsFactory {
 
-    public static final String VIRTUALENV_INTERPRETER = "pyleus_venv/bin/python";
+    public static final String VIRTUALENV_INTERPRETER_UNIX = "pyleus_venv/bin/python";
+    public static final String VIRTUALENV_INTERPRETER_WINDOWS = "pyleus_venv\\Scripts\\python.exe";
     public static final String MODULE_OPTION = "-m";
 
     private String[] buildCommandUnix(final String module, final Map<String, Object> argumentsMap,
@@ -25,7 +26,7 @@ public class PythonComponentsFactory {
 
         StringBuilder strBuf = new StringBuilder();
         // Done before launching any spout or bolt in order to cope with Storm permissions bug
-        strBuf.append(String.format("chmod 755 %s; %s", VIRTUALENV_INTERPRETER, VIRTUALENV_INTERPRETER));
+        strBuf.append(String.format("chmod 755 %s; %s", VIRTUALENV_INTERPRETER_UNIX, VIRTUALENV_INTERPRETER_UNIX));
         strBuf.append(String.format(" %s %s", MODULE_OPTION, module));
 
         if (argumentsMap != null) {
@@ -49,42 +50,43 @@ public class PythonComponentsFactory {
 
         return command;
     }
-    
+
     private String[] buildCommandWindows(final String module, final Map<String, Object> argumentsMap,
             final String loggingConfig, final String serializerConfig) {
-    	
+
     	final List<String> command = new ArrayList<String>();
-    	command.add(VIRTUALENV_INTERPRETER + ".exe");
+    	command.add("cmd.exe");
+    	command.add("/c");
+
+    	command.add(VIRTUALENV_INTERPRETER_WINDOWS);
     	command.add(MODULE_OPTION);
     	command.add(module);
-    	
+
     	if (argumentsMap != null) {
             Gson gson = new GsonBuilder().create();
             String json = gson.toJson(argumentsMap);
-
-            // json = json.replace("\"", "\\\"");
+            json = json.replace("\"", "\\\"");
             command.add("--options");
             command.add(json);
         }
-    	
+
     	{
             Map<String, Object> pyleusConfig = new HashMap<String, Object>();
             pyleusConfig.put("logging_config_path", loggingConfig);
             pyleusConfig.put("serializer", serializerConfig);
             Gson gson = new GsonBuilder().create();
             String json = gson.toJson(pyleusConfig);
-            
-            // json = json.replace("\"", "\\\"");
+            json = json.replace("\"", "\\\"");
             command.add("--pyleus-config");
             command.add(json);
         }
-    	
+
     	return command.toArray(new String[command.size()]);
     }
-    
+
     private String[] buildCommand(final String module, final Map<String, Object> argumentsMap,
             final String loggingConfig, final String serializerConfig) {
-    	
+
     	if(System.getProperty("os.name").toLowerCase().contains("windows")) {
     		return buildCommandWindows(module, argumentsMap, loggingConfig, serializerConfig);
     	} else {
