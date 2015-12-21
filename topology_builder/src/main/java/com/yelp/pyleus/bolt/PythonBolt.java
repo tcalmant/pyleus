f034f75e19b358fa8e9f2aec84902953e1a5d133
Thomas Calmant <thomas.calmant@gmail.com>
2015-12-21 13:56:51 +0100

    Added support for mixed operating systems
    - Allows to run the Nimbus server on Windows and components on Linux (for example)
    - Fixes #2

--- topology_builder/src/main/java/com/yelp/pyleus/bolt/PythonBolt.java
+++ topology_builder/src/main/java/com/yelp/pyleus/bolt/PythonBolt.java
@@ -1,11 +1,18 @@
 package com.yelp.pyleus.bolt;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Logger;
+
+import com.yelp.pyleus.PythonComponentsFactory;
 
 import backtype.storm.Config;
+import backtype.storm.task.OutputCollector;
 import backtype.storm.task.ShellBolt;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.IRichBolt;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
@@ -13,9 +20,37 @@ import backtype.storm.tuple.Fields;
 public class PythonBolt extends ShellBolt implements IRichBolt {
     protected Map<String, Object> outputFields;
     protected Float tickFreqSecs = null;
+    protected String[] localCommand;
 
     public PythonBolt(final String... command) {
-        super(command);
+        super();
+        localCommand = Arrays.copyOf(command, command.length);
+    }
+    
+    @Override
+    public void prepare(@SuppressWarnings("rawtypes") Map stormConf, TopologyContext context,
+    		OutputCollector collector) {
+    	
+    	try {
+    		// Update the field like a barbarian
+	    	Field privateCommandField = ShellBolt.class.getDeclaredField("_command");
+	    	privateCommandField.setAccessible(true);
+	    	privateCommandField.set(this, PythonComponentsFactory.buildCommand(localCommand));
+	    	
+    	} catch(SecurityException ex) {
+    		Logger.getGlobal().severe("Can't update the ShellBot command: " + ex);
+    	} catch (NoSuchFieldException ex) {
+    		// Can happen if ShellBot is updated
+    		Logger.getGlobal().severe("ShellBot seems to have been updated: " + ex);
+		} catch (IllegalArgumentException ex) {
+			// Won't happen
+			Logger.getGlobal().severe("Error updating the ShellBot command: " + ex);
+		} catch (IllegalAccessException ex) {
+			// Like security error
+			Logger.getGlobal().severe("Error updating the ShellBot command: " + ex);
+		}
+    	
+    	super.prepare(stormConf, context, collector);
     }
 
     public void setOutputFields(final Map<String, Object> outputFields) {
